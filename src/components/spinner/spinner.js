410731e1a93cdecd5425aa6797a0ac3d5a80eef1
Rostyslav Miniukov <thresh1337@gmail.com>
2021-01-11 17:50:55 +0200

    Adding proper spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,7 +3,20 @@ import React from 'react';
 import './spinner.css';
 
 const Spinner = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="text-center">
+      <div className="loadingio-spinner-wedges-18d42rwgrfe">
+        <div className="ldio-7ze60qkba0s">
+          <div>
+            <div><div></div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
